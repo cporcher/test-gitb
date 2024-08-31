4546602550078ffdcbf9db6ecf375f3db5f78f6e
Christophe PORCHER <christophe.porcher@soprasteria.com>
2024-08-31 15:54:32 +0200

    qz

--- MyApp.Domain/Class1.cs
+++ MyApp.Domain/Class1.cs
@@ -7,6 +7,27 @@ public class Class1
         // This line will not compile because Class1.Test1 is not async
         await Task.Delay(1000);
 
+        // test
         return;
     }
 }
+
+// Path: MyApp.UnitTests/UnitTest1.cs
+
+class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Arrange
+
+        sdfsd
+    }
+}
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
